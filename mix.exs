38a88eb7f4ad123414ede696da2b394deafdea45
libitx <libitx@protonmail.com>
2021-03-05 23:09:19 +0000

    Version bump.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Curvy.MixProject do
   def project do
     [
       app: :curvy,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
